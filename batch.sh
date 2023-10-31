99c887e029211b99e35c07dc039713cb848c2e1e
Katie <koates@udel.edu>
2023-10-30 23:29:06 -0400

    more changes

--- batch.sh
+++ batch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
-#SBATCH --job-name=<jobname>
+#SBATCH --job-name=<openMP>
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=<numthreads>
-#SBATCH --output <jobname>-job_%j.out
-#SBATCH --error <jobname>-job_%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --output <openMP>-job_%j.out
+#SBATCH --error <openMP>-job_%j.err
 #SBATCH --partition=standard
 
 # Bind your OpenMP threads
-export 4=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # Start my application
 srun <openMP>
